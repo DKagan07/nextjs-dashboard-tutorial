ee332fe75457ae776cd3cb36b58e11b79fb1c218
David Kagan <david.kagan07@gmail.com>
2024-09-23 10:57:01 -0400

    Add formatting and diagnostics with eslint and prettier

---
+++ .prettier.config.mjs
@@ -0,0 +1,15 @@
+import { IndentStyle } from "typescript";
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ *
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+};
+
+export default config;
